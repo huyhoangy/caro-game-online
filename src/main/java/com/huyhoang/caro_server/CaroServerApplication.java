31e2a4c61a9cbde01ae487c529ae4fc27a435456
huyhoangy <huyhoangwf@gmail.com>
2026-03-26 22:20:38 +0700

    khoi tao be

---
+++ src/main/java/com/huyhoang/caro_server/CaroServerApplication.java
@@ -0,0 +1,13 @@
+package com.huyhoang.caro_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaroServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaroServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/huyhoang/caro_server/CaroServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huyhoang.caro_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaroServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
